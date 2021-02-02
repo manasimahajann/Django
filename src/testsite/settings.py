4258a80a814ef66fddcdbba409b797428190b54f
Manasi Mahajan <manasimahajann@gmail.com>
2021-02-02 05:36:24 -0500

    added a project from git

--- src/testsite/settings.py
+++ src/testsite/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'testsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#             }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+
     }
 }
 
